69020d4b855c10745b3478c6eb4bd2976760016a
colderlord <colderlord@yandex.ru>
2022-11-21 16:47:25 +0400

    production build

---
+++ run.dev.ps1
@@ -0,0 +1,10 @@
+#Чтение параметров
+$values = Get-Content ".\.env" | Out-String | ConvertFrom-StringData
+
+$certName = $values.certName + ".pfx"
+$certPass = $values.certPass
+
+dotnet dev-certs https -ep $env:userprofile\.aspnet\https\$certName -p $certPass
+dotnet dev-certs https --trust
+
+docker-compose -f docker-compose.dev.yml up -d --build
