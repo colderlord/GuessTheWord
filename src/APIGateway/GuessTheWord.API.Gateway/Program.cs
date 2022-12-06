aee793672bbe164a436b289bc65ad4b51a10eb3e
colderlord <colderlord@yandex.ru>
2022-12-06 10:30:43 +0400

    Ocelot Gateway

---
+++ src/APIGateway/GuessTheWord.API.Gateway/Program.cs
@@ -0,0 +1,75 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using GuessTheWord.API.Gateway.Config;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MMLib.SwaggerForOcelot.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Polly;
+
+namespace GuessTheWord.API.Gateway
+{
+    /// <summary>
+    /// Класс-конфигуратор приложения
+    /// </summary>
+    public class Startup
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var routes = "Routes";
+
+            builder.Configuration.AddOcelotWithSwaggerSupport(options =>
+            {
+                options.Folder = routes;
+            });
+
+            builder.Services.AddOcelot(builder.Configuration).AddPolly();
+            builder.Services.AddSwaggerForOcelot(builder.Configuration);
+
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
+                .AddOcelot(routes, builder.Environment)
+                .AddEnvironmentVariables();
+
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+
+            // Swagger for ocelot
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+            }
+
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            await app.UseSwaggerForOcelotUI(options =>
+            {
+                options.PathToSwaggerGenerator = "/swagger/docs";
+                options.ReConfigureUpstreamSwaggerJson = AlterUpstream.AlterUpstreamSwaggerJson;
+            }).UseOcelot();
+
+            app.MapControllers();
+
+            await app.RunAsync();
+        }
+    }
+
+}
